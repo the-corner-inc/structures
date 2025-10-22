2ce6a2c5dd37dd10272fe6d9173b7980d6ceb0d1
Raphaël Balet <raphael.balet@bit.admin.ch>
2025-10-22 16:10:47 +0200

    perf(folder): when to select

--- src/app/pages/folders/explorer/folder/folder.component.ts
+++ src/app/pages/folders/explorer/folder/folder.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, inject, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, Input, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
 import { FOLDER_SETTINGS, SELECTED_ELEMENT, SELECTED_LIBRARY } from '@bases/base.token';
 import { IconNamePipe } from '@pages/folders/explorer/folder/icon-name.pipe';
@@ -14,26 +14,53 @@ import { FolderStructure } from '@pages/folders/folders';
     IconNamePipe,
   ],
 })
-export class FolderComponent {
+export class FolderComponent implements OnDestroy {
   readonly #folderSettings = inject(FOLDER_SETTINGS);
   readonly #selectedLibrary = inject(SELECTED_LIBRARY);
   readonly #selectedElement = inject(SELECTED_ELEMENT);
 
   readonly #router = inject(Router);
 
+  private _mouseMoveTimeout: ReturnType<typeof setTimeout> | null = null;
+
   iconBaseUrl = this.#folderSettings.getValue().iconBaseUrl;
 
   @Input({ required: true }) item!: FolderStructure;
   expanded = true;
 
-  toggle() {
+  ngOnDestroy(): void {
+    this._clearMouseMoveTimeout();
+  }
+
+  onClick() {
     this.expanded = !this.expanded;
   }
 
-  setFolderContent() {
+  onMouseMove() {
+    this._clearMouseMoveTimeout();
+
+    this._mouseMoveTimeout = setTimeout(() => {
+      this._navigate();
+    }, 150);
+  }
+
+  onMouseLeave() {
+    this._clearMouseMoveTimeout();
+  }
+
+  private _clearMouseMoveTimeout() {
+    if (this._mouseMoveTimeout) {
+      clearTimeout(this._mouseMoveTimeout);
+      this._mouseMoveTimeout = null;
+    }
+  }
+
+  private _navigate() {
     this.#selectedElement.next(this.item);
 
-    if (!this.#router.url.includes(this.item.name))
-      this.#router.navigate(['/folders', this.#selectedLibrary.getValue(), this.item.name]);
+    if (this.#router.url.includes(this.item.name)) return false;
+
+    this.#router.navigate(['/folders', this.#selectedLibrary.getValue(), this.item.name]);
+    return true;
   }
 }
